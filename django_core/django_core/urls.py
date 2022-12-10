08ec7c0eb95680143f38b07805850237b0a8a893
Артём <artemsam23@gmail.com>
2022-12-10 10:46:25 +0300

    fix: auth

--- django_core/django_core/urls.py
+++ django_core/django_core/urls.py
@@ -14,7 +14,6 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from unittest.mock import patch
-import os
 from django.contrib import admin
 from django.urls import include, path
 from django.urls import re_path
